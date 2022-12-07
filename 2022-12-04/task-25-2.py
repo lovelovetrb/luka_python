21fe1a7df9e0fc9e9490c0f6d6cce7c7c0f1fc12
Mizuki Baba <trombonelover8888@gmail.com>
2022-12-07 15:32:01 +0900

    complete qw-04

--- 2022-12-04/task-25-2.py
+++ 2022-12-04/task-25-2.py
@@ -1,23 +1,23 @@
-# 暗号化したい文字一文字
-M = "大東"
+# p = 307
+# q = 311
 
-separater = "-"
+# 公開鍵
+n = 95477
+#  n = p * q
+a = 94873
 
-# 暗号化された文字列を代入する変数(10進数)
-DecimalNumEnc = ""
+# 秘密鍵
+b = 7297
 
-# 文字列の長さを保jする変数
-index = len(M)
-counter = 0
+# 暗号化対象
+M = "a"
+# 暗号
+m = 0
 
-for s in M:
-    # 乱数と文章の文字コードから排他的論理和をとって暗号化(10進数)
-    # 配列の最後はseparaterを削除
-    if counter == index - 1:
-        DecimalNumEnc += str(x ^ ord(s))
-    else:
-        DecimalNumEnc += str(x ^ ord(s)) + separater
+# 暗号
+m = (ord(M) ** a) % n
 
-    counter += 1
+# 復号
+M = (m**b) % n
 
-
+print(chr(M))
