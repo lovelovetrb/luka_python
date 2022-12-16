91f2ec1c416d31c2c2689d398025189099d6dd6f
Mizuki Baba <trombonelover8888@gmail.com>
2022-12-16 22:25:02 +0900

    add 2022-12-11

---
+++ 2022-12-11/task2.py
@@ -0,0 +1,10 @@
+f = 0b000000000000111111
+b = 0b100000
+gf = open("./src/index.txt", "rb")
+for j in range(1000):
+    dr = [gf.read(1), gf.read(1), gf.read(1)]
+    d = (ord(dr[0]) << 16) + (ord(dr[1]) << 8) + ord(dr[2])
+    for i in range(4):
+        s = 6 * i
+        print(chr(((d & f << s) >> s) + b), end="")
+gf.close()
